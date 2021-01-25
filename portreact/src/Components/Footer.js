6f74009c42c95f65b34e522885ac4c2e6ca74aa7
Rachel <thelivedish@gmail.com>
2021-01-24 16:40:06 -0800

    add footer.js

---
+++ portreact/src/Components/Footer.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
+
+const FooterPage = () => {
+  return (
+    <MDBFooter color="blue" className="font-small pt-4 mt-4">
+      <MDBContainer fluid className="text-center text-md-left">
+        <MDBRow>
+          <MDBCol md="6">
+            <h5 className="title">Built in React.JS</h5>
+            <p>
+              Thank you for Stopping By!
+            </p>
+          </MDBCol>
+          <MDBCol md="6">
+            <h5 className="title">Links</h5>
+            <ul>
+              <li className="list-unstyled">
+                <a href="https://www.linkedin.com/in/rachel-s-40636296/">LinkedIn</a>
+              </li>
+              <li className="list-unstyled">
+                <a href="https://github.com/mikilao">Github</a>
+              </li>
+              <li className="list-unstyled">
+                <a href="#!">Link 3</a>
+              </li>
+              <li className="list-unstyled">
+                <a href="#!">Link 4</a>
+              </li>
+            </ul>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+      
+    </MDBFooter>
+  );
+}
+
+export default FooterPage;
